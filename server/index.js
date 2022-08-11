f4c223711809cd1e5f8a5d24e3628af90a467679
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-08-11 02:41:58 -0400

    Add listener for join room event.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 app.use(cors()); 
 
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-})
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  // Add a user to a room
+  socket.on('join_room', (data) => {
+    const { username, room } = data; // Data setn from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+  });
+});
 
 server.listen(4000, () => 'Server is running on port 4000');
