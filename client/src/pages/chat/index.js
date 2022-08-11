a773268b25865fc78ed5762bde0997071c1b8104
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-08-11 10:11:13 -0400

    Fixed spelling and errors.

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -1,11 +1,11 @@
 import styles from './styles.module.css';
-import Messagesreceived from './messages';
+import MessagesReceived from './messages';
 
 const Chat = ({ socket }) => {
   return (
     <div className={styles.chatContainer}>
       <div>
-        <Messagesreceived socket={socket} />
+        <MessagesReceived socket={socket} />
       </div>
     </div>
   );
